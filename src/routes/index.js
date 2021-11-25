fc8ebaf6683edffe05b98185bc25e900cd361903
Muratcan Payir <muratcanpayir@gmail.com>
2021-11-25 20:46:59 +0000

    Header Component Created

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,23 @@
-import React from 'react';
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
-import Home from '../pages/Home';
-import UnitDetails from '../pages/UnitDetails';
-import Units from '../pages/Units';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "../components/Header";
+import Home from "../pages/Home";
+import UnitDetails from "../pages/UnitDetails";
+import Units from "../pages/Units";
 
 function Routers() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/units" element={<Units/>}/>
-        <Route path="/details" element={<UnitDetails/>}/>
-      </Routes>
-
-    </Router>
-  )
+    <>
+      <Header />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/units" element={<Units />} />
+          <Route path="/details" element={<UnitDetails />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default Routers;
