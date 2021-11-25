115f91ac8c891d46ad0cb208a85e2b713b0ef582
Muratcan Payir <muratcanpayir@gmail.com>
2021-11-25 20:37:24 +0000

    Units page added

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import Home from '../pages/Home';
+import Units from '../pages/Units';
 
 function Routers() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/units" element={<Units/>}/>
       </Routes>
 
     </Router>
